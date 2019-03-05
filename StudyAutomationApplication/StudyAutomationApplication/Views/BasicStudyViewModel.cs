489054b8b80840b6c448be233c96b2c4a8b794ca
Michael Marzullo <michaelt.marzullo@gmail.com>
2019-03-05 13:32:45 -0500

    Added BasicStudy screen

--- StudyAutomationApplication/StudyAutomationApplication/Views/BasicStudyViewModel.cs
+++ StudyAutomationApplication/StudyAutomationApplication/Views/BasicStudyViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Caliburn.Micro;
 
 namespace SAA.Views
 {
-    class BasicStudyViewModel
+    public class BasicStudyViewModel : Screen
     {
+        public BasicStudyViewModel()
+        {
+        }
+
+        private string basecase;
+
+        public string Basecase
+        {
+            get
+            {
+                return basecase;
+            }
+            set
+            {
+                basecase = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public void OpenFileExplorer()
+        {
+            var dialog = new OpenFileDialog();
+
+            dialog.ShowDialog();
+
+            Basecase = dialog.FileName;
+        }
     }
 }
