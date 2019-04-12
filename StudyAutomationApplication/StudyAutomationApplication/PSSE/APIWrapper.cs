17c9fd4705c26e19992d92b6aac1d7ed31678175
marzullo <michaelt.marzullo@gmail.com>
2019-04-11 22:18:51 -0400

    Added APIWrapper class to run console commands Added CommandBuilder to create batch command strings

---
+++ StudyAutomationApplication/StudyAutomationApplication/PSSE/APIWrapper.cs
@@ -0,0 +1,36 @@
+﻿using SAA.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAA.PSSE
+{
+    public static class APIWrapper
+    {
+        private static Process commandProcess;
+
+        public static ShellViewModel ShellViewModel;
+
+        public static void Initialize()
+        {
+            commandProcess = new Process();
+            commandProcess.StartInfo.UseShellExecute = false;
+            commandProcess.StartInfo.RedirectStandardOutput = true;
+            commandProcess.StartInfo.CreateNoWindow = true;
+            commandProcess.StartInfo.FileName = "cmd.exe";
+        }
+
+        public static void RunCommand(string command)
+        {
+            Initialize();
+            commandProcess.StartInfo.Arguments = "/C " + command;
+            commandProcess.OutputDataReceived += (sender, args) => { ShellViewModel.AddOutput(args.Data); };
+            commandProcess.Start();
+            commandProcess.BeginOutputReadLine();
+            //commandProcess.WaitForExit();
+        }
+    }
+}
